1dc8dbfb159f5e960a88431890dbeb9701e9ef33
Ismail BASSAOUD <ismail.bassaoud@gmail.com>
2024-01-29 16:18:28 +0100

    :tada: Initial commit

---
+++ src/app/types/selected-type.ts
@@ -0,0 +1,4 @@
+export type IsSelectedType = {
+    isSelected?: boolean,
+
+}
---
+++ src/app/types/user-type.ts
@@ -0,0 +1,6 @@
+export type UserType = {
+    id: number,
+    firstname: string,
+    lastname: string,
+    role: string,
+}
